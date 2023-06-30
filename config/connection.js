36d8b4a80fd1ef8bdb59ea1694b671c44f286151
Kannan K Pillai <kpillaikannan62@gmail.com>
2023-06-30 14:09:00 +0530

    changed the database into atlas

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,17 @@ const state={
     db:null
 }
 module.exports.connect=function(done){
-    const url='mongodb://127.0.0.1:27017/:27017'
+    // const url='mongodb://127.0.0.1:27017/:27017'
+    const url= 'mongodb+srv://kpillaikannan62:2255A%40@cluster0.4tlrlkz.mongodb.net/'
     const dbname='shopping'
-
+    
     mongoClient.connect(url,(err,data)=>{
         if(err) return done(err)
         state.db=data.db(dbname)
         done()
     })
 }
-
+                  
 module.exports.get=()=>{
     return state.db
 }
